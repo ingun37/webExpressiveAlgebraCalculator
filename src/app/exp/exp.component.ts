8f8222c36d795c67ad49d93b61737253737cd373
ingun <ingun37@gmail.com>
2020-02-10 22:15:00 +0900

    diagram

--- src/app/exp/exp.component.ts
+++ src/app/exp/exp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { Input } from '@angular/core';
 import * as E from '../exp';
 import { MatDialog } from '@angular/material/dialog';
@@ -13,26 +13,26 @@ import { range, asSequence } from 'sequency';
   styleUrls: ['./exp.component.scss']
 })
 export class ExpComponent implements OnInit {
-  _lineage:E.Lineage
+  _lineage: E.Lineage
   @Input()
-  set lineage(l:E.Lineage) {
+  set lineage(l: E.Lineage) {
     this._lineage = l
 
     if (this.exp instanceof E.Add) {
       this.subviewLineages = associativeKidsOfAdd(l)
     } else {
-      this.subviewLineages = this.exp.kids.map((x,i)=>this.makeLineageForKid(i))
+      this.subviewLineages = this.exp.kids.map((x, i) => this.makeLineageForKid(i))
     }
   }
 
-  subviewLineages:E.Lineage[] 
+  subviewLineages: E.Lineage[]
 
-  @Output() changed = new EventEmitter<E.Lineage>(); 
+  @Output() changed = new EventEmitter<E.Lineage>();
 
-  get exp():E.Exp {
+  get exp(): E.Exp {
     return this._lineage.exp
   }
-  get mat():E.Matrix {
+  get mat(): E.Matrix {
     if (this._lineage.exp instanceof E.Matrix) {
       return this._lineage.exp as E.Matrix
     }
@@ -40,10 +40,10 @@ export class ExpComponent implements OnInit {
   }
   constructor(public dialog: MatDialog) { }
 
-  openDialogFor(lineage:E.Lineage): Promise<E.Exp> {
+  openDialogFor(lineage: E.Lineage): Promise<E.Exp> {
     return this.dialog.open(ApplyComponent, {
-      data: {lineage:lineage}
-    }).afterClosed().toPromise().then(x=> {
+      data: { lineage: lineage }
+    }).afterClosed().toPromise().then(x => {
       if (x) {
         return x as E.Exp
       }
@@ -59,14 +59,14 @@ export class ExpComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+
   }
 
-  cell2kid(r:number, c:number): number {
+  cell2kid(r: number, c: number): number {
     return r * this.mat.elements[0].length + c
   }
 
-  onCellClick(event:[number, number]) {
+  onCellClick(event: [number, number]) {
     let ri = event[0]
     let ci = event[1]
     let kidLineage = this.makeLineageForKid(this.cell2kid(ri, ci))
@@ -76,23 +76,23 @@ export class ExpComponent implements OnInit {
     })
   }
 
-  makeLineageForKid(kidIdx:number): E.Lineage {
+  makeLineageForKid(kidIdx: number): E.Lineage {
     let thisExp = this._lineage.exp
     let kidExp = this._lineage.exp.kids[kidIdx]
-    return new E.Lineage(this._lineage.chain.concat([kidIdx])  , kidExp)
+    return new E.Lineage(this._lineage.chain.concat([kidIdx]), kidExp)
   }
 
-  onKidChanged(lineage:E.Lineage) {
+  onKidChanged(lineage: E.Lineage) {
     console.log('kid change', lineage, this.changed)
     this.changed.emit(lineage)
   }
 
-  onMatrixResize(size:[number, number]) {
+  onMatrixResize(size: [number, number]) {
     let row = size[0]
     let col = size[1]
     let mat = this.mat
-    let newElements = range(0, row-1, 1).map(r=>{
-      return range(0, col-1, 1).map((c):E.Exp=>{
+    let newElements = range(0, row - 1, 1).map(r => {
+      return range(0, col - 1, 1).map((c): E.Exp => {
         if (r < mat.elements.length && c < mat.elements[0].length) {
           return mat.elements[r][c]
         } else {
@@ -103,19 +103,61 @@ export class ExpComponent implements OnInit {
     let l = new E.Lineage(this._lineage.chain, new E.Matrix(newElements))
     this.changed.emit(l)
   }
+  @ViewChildren("subview", { read: ElementRef }) subviews: QueryList<ElementRef>;
+  centers:number[] = []
+  end:number = 0
+  ngAfterViewChecked() {
+    
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      
+      let a = this.subviews.reduce((l:number[], r)=>{
+        return l.concat([
+          l[l.length-1] + r.nativeElement.getBoundingClientRect().width
+        ])
+      }, [0])
+      console.log(a)
+      if (a.length > 1) {
   
+        this.centers = range(0, a.length-2, 1).map(n=>Math.floor((a[n]+a[n+1])/2)).toArray()
+        this.end = this.centers[this.centers.length-1]
+        console.log(this.centers)
+      }
+    }, 1);
+    {
+      // let context = (this.canv.nativeElement as HTMLCanvasElement).getContext("2d")
+      // context.moveTo(20, 0)
+      // context.lineTo(20, 15)
+      // var accu = 0
+      // this.subviews.forEach(subview => {
+      //   let box = subview.nativeElement.getBoundingClientRect()
+      //   console.log(box)
+      //   let width = box.right - box.left
+      //   let x = accu + width/2
+        
+      //   context.lineTo(x, 15)
+      //   context.lineTo(x, 30)
+      //   context.moveTo(x, 15)
+      //   accu += width
+      // })
+
+      // context.stroke()
+    }
+  }
 }
 
-function associativeKidsOfAdd(l:E.Lineage):E.Lineage[] {
+function associativeKidsOfAdd(l: E.Lineage): E.Lineage[] {
   if (l.exp instanceof E.Add) {
-    let ffa = l.exp.kids.map((k, i)=>{
+    let ffa = l.exp.kids.map((k, i) => {
       return associativeKidsOfAdd(new E.Lineage(l.chain.concat([i]), k))
     })
-    return asSequence(ffa).flatMap(x=>asSequence(x)).toArray()
+    return asSequence(ffa).flatMap(x => asSequence(x)).toArray()
   } else {
     return [l]
   }
 }
+
 /*
 -  onHandleMove(event: { pointerPosition: { x: number, y: number } }) {
 -    let box = this.tbl.nativeElement.getBoundingClientRect()
